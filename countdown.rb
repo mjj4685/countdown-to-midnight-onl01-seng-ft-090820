48b7346ed1df4d5b07a7c3f0fe91d6a52454b600
MJ Johnson <mjohnson4685@gmail.com>
2020-09-11 03:36:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,6 @@
 #write your code here
 
-def countdown
-  number=10
+def countdown(number)
   while number>0
     puts "#{number} SECOND(S)"
     number-=1
@@ -9,8 +8,7 @@ def countdown
   puts "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-number=10
+def countdown_with_sleep(number)
 while number>0
   puts "#{number} SECOND(S)"
   number-=1
