4ae96d43913cb2ec48efaa918430f24e20c337d9
MJ Johnson <mjohnson4685@gmail.com>
2020-09-11 02:54:38 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,12 +1,14 @@
 #write your code here
 
-def countdown(number)
+def countdown
+  number=0
   while number>0
     puts "#{number} SECOND(S)"
     number+=10
+    puts "HAPPY NEW YEAR!"
   end
 
-end
+
 
 
 puts "HAPPY NEW YEAR!"
