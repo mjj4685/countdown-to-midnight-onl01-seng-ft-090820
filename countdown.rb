563931701c467a36c68b909294aa142263d21ab6
MJ Johnson <mjohnson4685@gmail.com>
2020-09-11 03:35:40 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown
   number=10
-  while number<0
+  while number>0
     puts "#{number} SECOND(S)"
     number-=1
   end
@@ -11,7 +11,7 @@ end
 
 def countdown_with_sleep
 number=10
-while number<0
+while number>0
   puts "#{number} SECOND(S)"
   number-=1
   sleep(1)
