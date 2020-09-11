303f0b4304bd6006c0bc01e92a380a9b9d4082c3
MJ Johnson <mjohnson4685@gmail.com>
2020-09-11 04:22:27 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(seconds)
   while seconds>0
-    puts "#{seconds} SECOND(S)"
+    puts "#{seconds} SECOND(S)!"
     seconds-=1
   end
    "HAPPY NEW YEAR!"
@@ -10,7 +10,7 @@ end
 
 def countdown_with_sleep(seconds)
 while seconds>0
-  puts "#{seconds} SECOND(S)"
+  puts "#{seconds} SECOND(S)!"
   sleep(1)
   seconds-=1
  end
