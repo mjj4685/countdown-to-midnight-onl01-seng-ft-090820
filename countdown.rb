8bced3d5cd161eacc0ccca25baf0998081ae634d
MJ Johnson <mjohnson4685@gmail.com>
2020-09-11 03:30:22 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,7 @@ def countdown
   number=0
   while number>0
     puts "#{number} SECOND(S)"
-    number-=10
+    number-=1
   end
   puts "HAPPY NEW YEAR!"
 end
@@ -13,7 +13,7 @@ def countdown_with_sleep
 number=0
 while number>0
   puts "#{number} SECOND(S)"
-  number-=10
+  number-=1
   sleep 1
 end
 puts "HAPPY NEW YEAR!"
